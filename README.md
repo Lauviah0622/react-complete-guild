29cb0d0b74d8cb689695d1cb65fc7e516498410f
Lauviah Fang <lauviah0622@gmail.com>
2019-10-24 21:27:43 +0800

    10/24 chapter-36

--- README.md
+++ README.md
@@ -1,3 +1,6 @@
+chapter.36 10/24
+
+
 This project was bootstrapped with [Create React App](https://github.com/facebookincubator/create-react-app).
 
 Below you will find some information on how to perform common tasks.<br>
--- src/App.js
+++ src/App.js
@@ -1,13 +1,18 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Person from './Person/Person';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <h1>HI, I'm react app</h1> 
+        <h1>HI, I'm react app</h1>
+        <p>Hello mother fucker</p> 
+        <Person />
+        <Person />
+        <Person />
       </div>
+      // React.createElement('div', {className: 'App'}, 'h1', 'h2', React.createElement('div', null, 'h1'))
     );
   }
 }
